4ebcc5c8ce9c2aaa34a2c4984b59c31b2a5557b4
lucasfvera <lucasfvera@hotmail.com>
2022-12-24 03:21:02 -0300

    Implement walkers input
    This commit creates a form to be able to input the
    number of walkers you want to show in the canvas.

--- index.js
+++ index.js
@@ -4,15 +4,30 @@
 const canvas = document.getElementById('stage');
 const context = canvas.getContext('2d');
 
+const span = document.getElementById('number-of-walkers')
+const form = document.getElementById('participants-form');
+
+
 let persons = {};
 
-const PERSONS = 2
+let PERSONS = 1;
 
-for(let i = 0; i < PERSONS; i++){
-	persons[i] = new Person(canvas)
+function createNPersons(n = 1){
+	console.log(`Creating ${PERSONS} persons`)
+	for(let i = 0; i < n; i++){
+		persons[i] = new Person(canvas)
+	}
+}
+
+function init(){
+	createNPersons(PERSONS);
 }
 
 function draw() {
+	if(PERSONS <= 0){
+		return
+	}
+	console.log(PERSONS)
 	context.clearRect(0, 0, canvas.width, canvas.height);
 	for(let i = 0; i < PERSONS; i++){
 		persons[i].draw()
@@ -22,3 +37,24 @@ function draw() {
 
 	window.requestAnimationFrame(draw);
 }
+
+function processForm(e) {
+    if (e.preventDefault) e.preventDefault();
+	const input = document.getElementById("number-of-participants")
+	if(!Number.isNaN(Number(input.value)) && input.value !== ''){
+		PERSONS = Number(input.value)
+		span.innerHTML = input.value
+		createNPersons(PERSONS);
+	}
+
+    // You must return false to prevent the default form behavior
+    return false;
+}
+
+if (form.attachEvent) {
+	form.attachEvent("submit", processForm);
+} else {
+	form.addEventListener("submit", processForm);
+}
+
+init();
